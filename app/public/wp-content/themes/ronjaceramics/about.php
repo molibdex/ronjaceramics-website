262f6bf316ab59948318caab2fc4ccffeffa3450
moli <maciejfleisz@gmail.com>
2020-02-15 23:55:07 +0100

    images and descrpitions  are now dynamic
    part 01

--- app/public/wp-content/themes/ronjaceramics/about.php
+++ app/public/wp-content/themes/ronjaceramics/about.php
@@ -3,6 +3,9 @@
   Template name: About us
 */
 
+$nasze = get_field('nasze');
+$onas = get_field('onas');
+
 get_header(); ?>
 <br>
 <br>
@@ -10,17 +13,17 @@ get_header(); ?>
 <div class="cardus">
     <div class="row no-gutters">
         <div class="col-md-5">
-            <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/aboutus.png" alt="test"class="card-img-top h-100" alt="...">
+          <!-- If user uplodaed an image -->
+          <?php if( !empty ($nasze)) : ?>
+            <img class="img-fluid ard-img-top h-100" src="<?php echo $nasze['url']; ?>" alt="<?php echo $nasze['alt']; ?>">
+          <?php endif; ?>
+
         </div>
         <div class="col-md-7">
             <div class="card-body">
 
               <p class="card-text">
-              Ronja Ceramics to Wiktoria Rutkowska i Karolina Grzegorczyk. Spotkałyśmy się  i bardzo się lubimy. Lubimy też lepić. Dla Ciebie.
-              I lubimy moment kiedy nasze pomysły, przegadane godziny i próbki zamieniają się w przedmiot, którym możesz się cieszyć. Wtedy my też się cieszymy.
-              Staramy się żeby nasze naczynia były minimalistyczne, proste i spokojne, żeby mogły służyć w różnych przestrzeniach: w kuchni, w kawiarni, na łące.Jesteśmy absolwentkami Uniwersytetu Ludowego Rzemiosła Artystycznego i członkiniami rzeszowskiej Izby Rzemieślniczej. Karolina jest czeladniczką, Wiktoria mistrzynią.
-              Poza tym Karolina jest historyczką sztuki, lubi kawę i mieszać gips łapą.
-              Wiktoria jest edukatorką artystyczną, pedagożką teatru i animatorką, lubi Gwiezdne Wojny i toczenie na kole.
+                <?php echo $onas; ?>
               </p>
             </div>
         </div>
