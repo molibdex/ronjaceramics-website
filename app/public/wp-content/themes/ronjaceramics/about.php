4faab606f5715377b8715aa4c2384bd668168a99
moli <maciejfleisz@gmail.com>
2020-04-19 00:02:20 +0200

    about us
    many changes in display

--- app/public/wp-content/themes/ronjaceramics/about.php
+++ app/public/wp-content/themes/ronjaceramics/about.php
@@ -8,23 +8,14 @@ $onas = get_field('onas');
 
 get_header(); ?>
 
-<div class="cardus">
-    <div class="row no-gutters">
-        <div class="col-md-5">
-          <!-- If user uplodaed an image -->
-          <?php if( !empty ($nasze)) : ?>
-            <img class="img-fluid ard-img-top h-100 onas" src="<?php echo $nasze['url']; ?>" alt="<?php echo $nasze['alt']; ?>">
-          <?php endif; ?>
-        </div>
-        <div class="col-md-7">
-            <div class="card-body">
-              <p class="card-text">
-                <?php echo $onas; ?>
-              </p>
-            </div>
-        </div>
-    </div>
-</div>
+<div class="clearfix cardus">
+  <?php if( !empty ($nasze)) : ?>
+  <img src="<?php echo $nasze['url']; ?>" alt="<?php echo $nasze['alt']; ?>" class="img-fluid float-left onr mr-4 nopadding">
 
+  <?php endif; ?>
+
+  <p class=""><?php echo $onas; ?></p>
+
+</div>
 
 <?php get_footer(); ?>
