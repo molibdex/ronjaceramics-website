ce6f5fba9bba3a7c6ac1c867f70650d7471853c9
moli <maciejfleisz@gmail.com>
2020-04-19 18:31:24 +0200

    single product v1
    display changes

--- app/public/wp-content/themes/ronjaceramics/contact.php
+++ app/public/wp-content/themes/ronjaceramics/contact.php
@@ -5,33 +5,17 @@
 
 
 get_header(); ?>
-<br>
-<br>
-<br>
+
+<div class="clearfix cardus">
+
+  <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/map.png"  class="img-fluid float-left onr mr-4 nopadding">
 
 
-<div class="cardwork" style="max-width: 1000px;">
-    <div class="row no-gutters">
-        <div class="col-md-5">
 
-            <img class="img-fluid ard-img-top h-100 " src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/map.png">
+  <p><i class="fa fa-address-card mr-3"></i>Stalowa 15, 02-031 Warszawa</p>
+  <p><i class="fa fa-phone mr-3"></i>00 000 00 00</p>
+  <p><i class="fa fa-envelope mr-3"></i>ronjaceramics@gmail.com</p>
 
-        </div>
-        <div class="col-md-7">
-            <div class="card-bodywork">
-              <br>
-              <br>
-              <br>
-              <br>
-              <p><i class="fa fa-address-card mr-3"></i>Stalowa 15, 02-031 Warszawa</p>
-              <p><i class="fa fa-phone mr-3"></i>00 000 00 00</p>
-              <p><i class="fa fa-envelope mr-3"></i>ronjaceramics@gmail.com</p>
-              <br>
-              <br>
-              <br>
-              <br>
-            </div>
-        </div>
-    </div>
 </div>
+
 <?php get_footer(); ?>
